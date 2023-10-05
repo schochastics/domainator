e08d4b21f46f7404f8e37bf7b1864ec9ddfd3011
schochastics <david@schochastics.net>
2023-10-05 13:13:51 +0200

    added lists

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' News Types
+#'
+#' Classification of domains into different news types
+#' @references
+#'  Stier, S., Mangold, F., Scharkow, M., & Breuer, J. (2022). Post Post-Broadcast Democracy? News Exposure in the Age of Online Intermediaries. American Political Science Review, 116(2), 768-774.
+"news_types"
+
+#' Domain list
+#' classification of domains into news,portals, search, and social media
+#' @references
+#'  Stier, S., Mangold, F., Scharkow, M., & Breuer, J. (2022). Post Post-Broadcast Democracy? News Exposure in the Age of Online Intermediaries. American Political Science Review, 116(2), 768-774.
+"domain_list"
